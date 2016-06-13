9dc1634c48b471cb1fca82b4c9373a5e89cd0d8b
kop <thekop@ubuntu.ubuntu-domain>
2016-06-13 13:21:52 +0700

    change api key sendgrid

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -34,7 +34,7 @@ class AdminMailer < ActionMailer::Base
 
   def sendgrid_client
     client = SendGrid::Client.new do |c|
-      c.api_key = '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'
+      c.api_key = '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'
     end
     mail = SendGrid::Mail.new do |m|
       m.to = 'abc1zbq@gmail.com'
