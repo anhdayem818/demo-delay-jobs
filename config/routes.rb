9131592e1ccb12dd557157a01d9167c4c3e43b1f
kop <thekop@ubuntu.ubuntu-domain>
2016-06-13 14:40:57 +0700

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   end
   scope '/hooks', :controller => :hook do
     get :survery_created_callback
+    post :survery_created_callback
   end
   root 'newsletters#index'
   # The priority is based upon order of creation: first created -> highest priority.
