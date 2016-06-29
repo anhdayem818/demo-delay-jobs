71e82db0915a87a5f040dbd793a9f5597a575a75
Bach Tran Nguyen <trannguyenbach1992@gmail.com>
2016-06-29 10:18:39 +0700

    update

--- app/controllers/hook_controller.rb
+++ app/controllers/hook_controller.rb
@@ -5,19 +5,23 @@ class HookController < ApplicationController
   end
 
   def survery_created_callback
-    puts params["hook"]["_json"]
-    results = params["hook"]["_json"][0]
-    email_id = results["_id"]
-    event = results["event"]
-    email_info = EmailInfo.find_by_email_id(email_id)
-    case event
-    when "open"
-      email_info.opened
-    when "click"
-      email_info.clicked
+    begin
+      puts params["hook"]["_json"]
+      results = params["hook"]["_json"][0]
+      email_id = results["_id"]
+      event = results["event"]
+      email_info = EmailInfo.find_by_email_id(email_id)
+      break unless email_info.present?
+      case event
+      when "open"
+        email_info.opened
+      when "click"
+        email_info.clicked
+      end
+    rescue Exception => e
     end
     render :nothing => true
-
+    
   end
 
 end
